8289473cfe0b629001c3b554da567217ae12e012
Alexander Olmedo <olmedo2408@gmail.com>
2018-10-10 08:55:44 -0500

    Ifs

---
+++ 01-Python/02_condicional.py
@@ -0,0 +1,15 @@
+# Truthy
+# Falsy
+
+if True:
+    print("Es verdad")
+else:
+    print("No es verdad")
+
+nombre = "Alexander"
+diccionario = {"a"}
+
+if diccionario:
+    print("Si")
+else:
+    print("No")
