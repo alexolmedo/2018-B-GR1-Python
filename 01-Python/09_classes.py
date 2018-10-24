a3455028f3dc837130920a116ae666e15ed0e05c
Alexander Olmedo <olmedo2408@gmail.com>
2018-10-24 08:08:33 -0500

    Classes

--- 01-Python/09_classes.py
+++ 01-Python/09_classes.py
@@ -1,14 +1,4 @@
-class Auto:
-    pass
-
-
-bm = Auto()
-
-print(bm)
-
-
 class Escuela:
-
     # valor_categoria = 4
     __ciudad = 'Quito'  # atributo privado
     pais = 'Ecuador'  # atributo publico
@@ -33,8 +23,51 @@ class Escuela:
         return 'Escuela'
 
 
-# twa = Escuela('Theodoro Winword Anderson')
-# twa.valor_categoria = 2
-# twa.saludar()
-# print(twa.categoria())
+twa = Escuela('Theodoro Winword Anderson')
+twa.valor_categoria = 2
+twa.saludar()
+print(twa.categoria())
 
+
+class Auto:
+    _ensamblado = 'Quito'
+    numero_asientos = 5
+
+    def __init__(self, nombre, color):
+        self.nombre = nombre
+        self.color = color
+
+    def __init__(self, nombre, color, color_techo=''):
+        self.nombre = nombre
+        self.color = color
+        self.color_techo = color_techo
+
+    def cambiar_ensamblado(self, ensamblado):
+        self._ensamblado = ensamblado
+
+    def __maximo_numero_pasajeros(self):
+        return self.numero_asientos + 3
+
+    def __str__(self):
+        return (f"{self.nombre}\n"
+                f"{self.color}\n"
+                f"{self.color_techo}\n"
+                f"{self.numero_asientos}\n"
+                f"{self._ensamblado}\n"
+                f"{self.__maximo_numero_pasajeros()}\n")
+
+
+bmw = Auto('Blanco', 'Version 1')
+print(bmw)
+
+
+class Hyundai(Auto):
+
+    def __init__(self, color, nombre):
+        super().__init__(color=color, nombre=nombre)
+        print('constructor')
+        print(self._ensamblado)
+
+
+mi_carro = Hyundai('Negro', 'Santa fe')
+print(mi_carro)
