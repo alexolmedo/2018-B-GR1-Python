e39eec40dca9602efdfaa6eeaeeebe34fc170c89
Alexander Olmedo <olmedo2408@gmail.com>
2018-11-17 10:23:21 -0500

    Spyder

---
+++ 03-Spyder/leer_csv.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 10:04:09 2018
+
+@author: Alexander
+"""
+import pandas as pd
+import os 
+
+CSV_PATH ="C://Users//Alexander//Documents//Python//olmedo-vinueza-alexander-daniel//03-Spyder//data//artwork_data.csv"
+
+# 1) Archivos texto
+# 2) Binary Files
+
+data_frame_artwork = pd.read_csv(
+        CSV_PATH,
+        nrows=5,
+        index_col= 'id',
+        usecols=['id','artist'])
+
+columnas_a_utilizar = ['id','artist','title',
+                       'medium','year',
+                       'acquisitionYear','height',
+                       'width','units']
+
+data_frame_completo_artwork = pd.read_csv(
+        CSV_PATH,
+        index_col= 'id',
+        usecols=columnas_a_utilizar)
+
+data_frame_completo_artwork.shape
