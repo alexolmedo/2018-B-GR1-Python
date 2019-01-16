df2b977973025f5f9d2d7d3097c7980a6b6fe1ea
Alexander Olmedo <olmedo2408@gmail.com>
2019-01-16 08:52:35 -0500

    pipelines

--- 05-Scrapy/02-spider/python_04/python_04/pipelines.py
+++ 05-Scrapy/02-spider/python_04/python_04/pipelines.py
@@ -5,7 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-
-class Python04Pipeline(object):
+class ValidarMacbook(object):
     def process_item(self, item, spider):
+        if ('macbook' not in item['titulo'].lower() or float(item['precio'].replace('$','').replace(',','')) < 200.0):
+            item['titulo'] = 'No-Macbook'
         return item
+
+class ValidarPrecio(object):
+    def process_item(self, item, spider):
+        precio = float(item['precio'].replace('$','').replace(',',''))
+        if (precio < 900.0):
+            item['precio'] = 'Muy Barato'
+        return item
+
+# class MarkarComoViable(object):
+#   def process_item(self, item, spider):
+#     if item['titulo'] != 'No-Macbook' and item['precio'] != 'Inasequible':
+#       print("\n \nItem encontrado")
+#       print("Titulo", item['titulo'])
+#       print("Precio", item['precio'])
+#       print("Link", item['link'])
+#       print("\n")
+#     return item
