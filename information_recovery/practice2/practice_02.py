85bc2542768459965265e750e4f6cdc59925e3c7
Alexander Olmedo <olmedo2408@gmail.com>
2019-03-17 21:23:58 -0500

    exercise 2

---
+++ information_recovery/practice2/practice_02.py
@@ -0,0 +1,73 @@
+from html.parser import HTMLParser
+from collections import OrderedDict
+import glob
+import errno
+from os import listdir
+
+class MyHTMLParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.recording = 0
+        self.data = []
+        self.currentTag = ''
+        self.currentDoc = ''
+
+    def handle_starttag(self, tag, attrs):
+        self.currentTag = tag
+        if self.recording:
+            self.recording += 1
+            return
+        else:
+            self.recording = 1
+            return
+
+    def handle_endtag(self, tag):
+        self.currentTag = ''
+        self.recording -= 1
+
+    def handle_data(self, data):
+        if (self.currentTag == 'docno'):
+            self.currentDoc = data
+        else:
+            wordlist = str(data).split(" ")
+            for word in wordlist:
+                # check if word already exists in index
+                if word in index:
+                    listTemp = list(index[word])
+                    # check if document already exists for certain word
+                    duplicatedDoc = False
+                    for doc in listTemp:
+                        if (doc[1] == self.currentDoc):
+                            duplicatedDoc = True
+                            doc[0] += 1
+                    if (duplicatedDoc):
+                        index[word] = listTemp
+                    else:
+                        listTemp.append([1, self.currentDoc])
+                        index[word] = listTemp
+                # if it doesn't exist create new word in index
+                else:
+                    index[word] = [[1, self.currentDoc]]
+
+
+index = {}
+parser = MyHTMLParser()
+
+with open("Practice_02_data/01-Text_Only-Ascii-Coll-1-10-NoSem", 'r', encoding='utf8') as myfile:
+    data = myfile.read().replace('\n', '')
+
+data = data.replace('’', ' ')
+data = data.replace('  ', ' ')
+data = data.lower()
+
+parser.feed(data)
+
+sortedIndex = OrderedDict(sorted(index.items(), key=lambda x: x[0]))
+
+with open('practice_01_results.txt', 'w') as file:
+    for key, value in sortedIndex.items():
+        file.write(key+' = \n\t'+str(value)+'\n')
+
+
+
